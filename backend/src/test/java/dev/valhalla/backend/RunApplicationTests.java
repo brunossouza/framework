7cab6bc51120a97aed53f0fc7cd101d884e45866
brunossouza <brunos.souza2@gmail.com>
2021-08-20 17:40:00 -0300

    first commit

---
+++ backend/src/test/java/dev/valhalla/backend/RunApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.valhalla.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
