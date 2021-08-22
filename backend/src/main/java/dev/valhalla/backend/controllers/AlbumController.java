27e9b94a40e275dc3bc286ba7c5edec493c3508f
Bruno <brunos.souza2@gmail.com>
2021-08-22 19:08:12 -0300

    Update AlbumController.java

--- backend/src/main/java/dev/valhalla/backend/controllers/AlbumController.java
+++ backend/src/main/java/dev/valhalla/backend/controllers/AlbumController.java
@@ -1,18 +1,15 @@
 package dev.valhalla.backend.controllers;
 
-import dev.valhalla.backend.models.Albums;
 import dev.valhalla.backend.services.AlbumService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/albums")
